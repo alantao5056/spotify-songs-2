d52308a04ba1df85b7bdd94067185c6d3b8a6679
alantao5056 <alantaolr@outlook.com>
2024-08-02 09:43:09 -0400

    remove _site add data

---
+++ model.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(tidybayes)
+library(gtsummary)
+library(brms)
+
+spotify <- read_csv("src/data/spotify_30000.csv")
+
+fit <- brm(
+  data = spotify,
+  family = gaussian(),
+  formula = track_popularity ~ playlist_genre + danceability + speechiness + valence + tempo,
+  silent = 2,
+  refresh = 0,
+  seed = 9
+)
+
